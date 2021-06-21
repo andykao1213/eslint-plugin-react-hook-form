25f580a0f82d9b06c3b59fc42bc0b34b27ed20aa
Andrew Kao <andykao1213@gmail.com>
2021-06-21 21:57:26 +0800

    test(no-access-control): add test cases

---
+++ tests/lib/rules/no-access-control.js
@@ -0,0 +1,82 @@
+/**
+ * @fileoverview Avoid accessing the properties of control
+ * @author Andrew Kao
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-access-control"),
+  RuleTester = require("eslint").RuleTester;
+
+const normalizeIndent = require("../utils/normalizeIndent");
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+ruleTester.run("no-access-control", rule, {
+  valid: [
+    {
+      code: normalizeIndent`
+        function Component(){
+            const {control} = useForm();
+            return <Controller control={control} />
+        }
+      `,
+    },
+    {
+      code: normalizeIndent`
+        function Component(){
+            const {control: c} = useForm();
+            return <Controller control={c} />
+        }
+    `,
+    },
+  ],
+  invalid: [
+    {
+      code: normalizeIndent`
+        function Component(){
+            const {control} = useForm();
+            console.log(control.defaultValuesRef);
+        }
+    `,
+      errors: [
+        {
+          messageId: "noAccessControl",
+          line: 4,
+          column: 25,
+          endLine: 4,
+          endColumn: 41,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component(){
+            const {control:c} = useForm();
+            console.log(c.defaultValuesRef);
+        }
+      `,
+      errors: [
+        {
+          messageId: "noAccessControl",
+          line: 4,
+          column: 19,
+          endLine: 4,
+          endColumn: 35,
+        },
+      ],
+    },
+  ],
+});
