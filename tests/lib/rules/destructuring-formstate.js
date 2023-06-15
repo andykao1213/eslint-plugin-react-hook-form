b708dc906dd579adaedc42a13ff078329b2cae80
Yuya Tanaka <yuya.presto@gmail.com>
2023-06-15 18:34:21 +0900

    feat: check for `useController()`

--- tests/lib/rules/destructuring-formstate.js
+++ tests/lib/rules/destructuring-formstate.js
@@ -31,8 +31,20 @@ ruleTester.run("destructuring-formstate", rule, {
     {
       code: normalizeIndent`
         function Component() {
+          const {formState: {isDirty}, fieldState: {isTouched}} = useController();
+          console.log(isDirty);
+          console.log(isTouched);
+          return null;
+        }
+      `,
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
           const formState = {isDirty: true};
+          const fieldState = {isTouched: true};
           console.log(formState.isDirty);
+          console.log(fieldState.isTouched);
           return null;
         }
     `,
@@ -120,6 +132,58 @@ ruleTester.run("destructuring-formstate", rule, {
           column: 18,
           endLine: 4,
           endColumn: 25,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {formState, fieldState} = useController();
+          console.log(formState.isDirty);
+          console.log(fieldState.isTouched);
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useDestructure",
+          line: 4,
+          column: 25,
+          endLine: 4,
+          endColumn: 32,
+        },
+        {
+          messageId: "useDestructure",
+          line: 5,
+          column: 26,
+          endLine: 5,
+          endColumn: 35,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {formState: fs1, fieldState: fs2} = useController();
+          console.log(fs1.isDirty);
+          console.log(fs2.isTouched);
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useDestructure",
+          line: 4,
+          column: 19,
+          endLine: 4,
+          endColumn: 26,
+        },
+        {
+          messageId: "useDestructure",
+          line: 5,
+          column: 19,
+          endLine: 5,
+          endColumn: 28,
         },
       ],
     },
