efd04cef27ca0c6b870b1f272df68dbaa5cec8f1
tatsuya.asami <46734354+tatsuya-asami@users.noreply.github.com>
2025-05-11 23:09:23 +0900

    feat(no-use-watch): Add rule prohibiting use of watch (#26)
    * feat: add no-use-watch rule to enforce useWatch over watch
    
    * feat: add react-compiler config with no-use-watch rule
    
    * fix: correct indentation in react-compiler configuration
    
    * docs: add documentation for no-use-watch rule in React Compiler
    
    * docs: add no-use-watch rule to README
    
    * fix: typo
    
    * feat: enhance no-use-watch rule with additional checks for useForm and useFormContext
    
    ---------
    
    Co-authored-by: tatsuya-asami <asami.tatsuya@commmune.jp>

---
+++ tests/lib/rules/no-use-watch.js
@@ -0,0 +1,161 @@
+/**
+ * @fileoverview Use useWatch instead of watch. This ensures the hook has subscribed to the state changes when using React Compiler.
+ * @author tatsuya.asami
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-use-watch"),
+  RuleTester = require("eslint").RuleTester;
+
+const normalizeIndent = require("../utils/normalizeIndent");
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 9 } });
+ruleTester.run("no-use-watch", rule, {
+  valid: [
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {control} = useForm();
+          useWatch(control);
+          return null;
+        }
+    `,
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {control} = useFormState();
+          useWatch(control);
+          return null;
+        }
+      `,
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const watch = 'watch';
+          console.log(watch);
+          return null;
+        }
+      `,
+    },
+  ],
+
+  invalid: [
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {watch} = useForm();
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 3,
+          column: 10,
+          endLine: 3,
+          endColumn: 15,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const formMethods = useForm();
+          console.log(formMethods.watch());
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 4,
+          column: 27,
+          endLine: 4,
+          endColumn: 32,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const {watch} = useFormContext();
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 3,
+          column: 10,
+          endLine: 3,
+          endColumn: 15,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const formContext = useFormContext();
+          console.log(formContext.watch());
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 4,
+          column: 27,
+          endLine: 4,
+          endColumn: 32,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const form = useForm();
+          const {watch} = form;
+          console.log(watch());
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 4,
+          column: 10,
+          endLine: 4,
+          endColumn: 15,
+        },
+      ],
+    },
+    {
+      code: normalizeIndent`
+        function Component() {
+          const formContext = useFormContext();
+          const {watch} = formContext;
+          console.log(watch());
+          return null;
+        }
+      `,
+      errors: [
+        {
+          messageId: "useUseWatch",
+          line: 4,
+          column: 10,
+          endLine: 4,
+          endColumn: 15,
+        },
+      ],
+    },
+  ],
+});
